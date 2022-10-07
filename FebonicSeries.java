b6a91a57302f1847da8fef86ed262bc62b4bef68
Jyothsnamukku <114405789+Jyothsnamukku@users.noreply.github.com>
2022-10-07 17:43:03 +0530

    javaprogramms

---
+++ FebonicSeries.java
@@ -0,0 +1,17 @@
+public class FebonicSeries {
+    public static void main(String[] args) {
+
+
+        int x=0;
+        int y=1;
+        int z=0;
+
+      for (int i=0;i<10;i++){
+
+          x=y+z;
+          y=z;
+          z=x;
+          System.out.println(y);
+      }
+    }
+}
---
+++ ReverseATeowNumberWithOut.java
@@ -0,0 +1,12 @@
+public class ReverseATeowNumberWithOut {
+
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.printf(a+"     "+b);
+    }
+}
